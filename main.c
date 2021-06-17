8dad0d1af9f7e38a6c0b0c94e5e1b2b36ed00f7f
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-06-17 09:36:25 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define TRUE (1 == 1)
 #define FALSE (1 == 0)
 
-#define FUNCTION_COUNT 2
+#define FUNCTION_COUNT 3
 #define MAX_FUNCTION_NAME_LENGTH 247
 #define MAX_FUNCTION_ARG_LENGTH 127
 
@@ -19,6 +19,16 @@ char *strr(char *str, char *output, int x, int y)
         output[i - (x + 1)] = str[i];
     }
     return output;
+}
+
+void clear_str(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        str[i] = '\0';
+        i++;
+    }
 }
 
 int is_string(char *str)
@@ -65,21 +75,32 @@ void lang_printi(int i)
     printf("%d\n", i);
 }
 
+void lang_lens(char *s)
+{
+    char output[MAX_STRING_LENGTH] = "\0";
+    if (is_string(s))
+    {
+        strr(s, output, 0, strlen(s) - 1);
+        printf("%ld\n", strlen(output));
+    }
+}
+
 int main()
 {
     int i;
     char function_names[FUNCTION_COUNT][MAX_FUNCTION_NAME_LENGTH] = {
         "prints",
-        "printi"
+        "printi",
+        "lens"
     };
     char input[MAX_STRING_LENGTH];
-    char output[MAX_STRING_LENGTH];
     char function_name[MAX_FUNCTION_NAME_LENGTH] = "\0";
     char function_arg[MAX_FUNCTION_ARG_LENGTH] = "\0";
     printf("> ");
     scanf("%s", input);
     for (i = 0; i < FUNCTION_COUNT; i++)
     {
+        clear_str(function_name);
         strr(input, function_name, -1, strlen(function_names[i]));
         strr(input, function_arg, strlen(function_names[i]), strlen(input) - 1);
         if (strcmp(function_name, function_names[i]) == FALSE)
@@ -96,6 +117,9 @@ int main()
                         {
                             lang_printi(atoi(function_arg));
                         }
+                        break;
+                    case 2:
+                        lang_lens(function_arg);
                         break;
                 }
             }
