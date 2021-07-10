e23264a3f3c3d8fdc4883314f292c0655bacb63f
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-07-10 16:10:30 -0400

    Create lang.c

---
+++ lang/lang.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../constants.h"
+#include "../functions.h"
+
+void lang_prints(char *s)
+{
+    char output[MAX_STRING_LENGTH] = "\0";
+    if (is_string(s))
+    {
+        strr(s, output, 0, strlen(s) - 1);
+        printf("%s\n", output);
+    }
+}
+
+void lang_printi(int i)
+{
+    printf("%d\n", i);
+}
+
+void lang_system(char *s)
+{
+    char output[MAX_STRING_LENGTH] = "\0";
+    if (is_string(s))
+    {
+        strr(s, output, 0, strlen(s) - 1);
+        system(output);
+    }
+}
